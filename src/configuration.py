c81792a0fadaf3ce920a1ac4e4ee476f69a7254b
jonathankovler <kovler.jonathan@gmail.com>
2024-09-23 11:31:24 +0300

    adding more src files

---
+++ src/configuration.py
@@ -0,0 +1,6 @@
+PATH_TO_TABLE = r'C:\Users\Yoni\PycharmProjects\news-headlines-sentiment-analysis\data\ynet_news_headlines.csv'
+WEB_URL = 'https://www.ynet.co.il/news/category/184'
+PATH_TO_PROCESSED_TABLE = r'C:\Users\Yoni\PycharmProjects\news-headlines-sentiment-analysis\data\ynet_news_headlines_processed.csv'
+PATH_TO_MY_MODEL = "avichr/heBERT_sentiment_analysis"
+PATH_TO_TOKENIZER = "avichr/heBERT_sentiment_analysis"
+HEBREW_STOP_WORDS = r'C:\Users\Yoni\PycharmProjects\hebrew_stop_words.txt'
---
+++ src/sentiment_analysis.py
@@ -0,0 +1,34 @@
+from transformers import AutoTokenizer, AutoModel, pipeline
+import pandas as pd
+import configuration
+
+sentiment_analysis = pipeline(
+    "sentiment-analysis",
+    model=configuration.PATH_TO_TOKENIZER,
+    tokenizer=configuration.PATH_TO_TOKENIZER,
+    return_all_scores=False
+)
+
+df = pd.read_csv(
+    configuration.PATH_TO_TABLE,
+    encoding='utf-8-sig')
+
+
+# Function to add sentiment to the csv file
+def analyze_sentiment_and_append(row):
+    headline = row['Headline'][0]
+    sent_analysis = sentiment_analysis(headline)[0]
+    # max_result = max(sent_analysis[0], key=lambda x: x['score'])
+    row['Sentiment'] = sent_analysis['label']
+    row['Score'] = sent_analysis['score']
+    return row
+
+
+if __name__ == '__main__':
+    try:
+        df = df.apply(analyze_sentiment_and_append, axis=1)
+        df.to_csv(configuration.PATH_TO_PROCESSED_TABLE, index=False, encoding='utf-8-sig')
+        print("created")
+
+    except Exception as ex:
+        print(ex)
